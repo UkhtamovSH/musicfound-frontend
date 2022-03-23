c9f8bb7cc28a893130203405cfd256899ed1c312
UkhtamovSH <uktamovshokhrukh@gmail.com>
2022-03-23 17:29:52 +0500

    landing page in process 2

--- src/components/sections/LayoutUserSide/FooterFixed.jsx
+++ src/components/sections/LayoutUserSide/FooterFixed.jsx
@@ -16,49 +16,83 @@ export const FooterFixed = () => {
   return (
     <FooterFixedMain>
       <ContainerStyle>
-        <span className='icon icon-skipBack' />
-        {play ? (
-          <span className='icon icon-play' onClick={handlePlay} />
-        ) : (
-          <span className='icon icon-pause' onClick={handlePlay} />
-        )}
-        <span className='icon icon-skipForward' />
-        <span className='icon icon-download' />
-        <span className='icon icon-starFill' />
+        <div className='mid2'>
+          <div className='mid2Sub1'>0:00</div>
+          <div className='mid2Sub2'>
+            ee kasdjasjkdakj kaksdjkaskjd akjsdjkaskj jkajksdjkasjkj kjkasjkdjk
+            jkasjkdj
+          </div>
+          <div className='mid2Sub3'>0:00</div>
+        </div>
+        <div className='footerFixedFlex'>
+          <div className=''>
+            <span className='icon icon-skipBack' />
+            {play ? (
+              <span className='icon icon-play' onClick={handlePlay} />
+            ) : (
+              <span className='icon icon-pause' onClick={handlePlay} />
+            )}
+            <span className='icon icon-skipForward' />
+          </div>
+          <div className='mid'>
+            <div className='midSub1'>0:00</div>
+            <div className='midSub2'>
+              ee kasdjasjkdakj kaksdjkaskjd akjsdjkaskj jkajksdjkasjkj
+              kjkasjkdjk jkasjkdj
+            </div>
+            <div className='midSub3'>0:00</div>
+          </div>
+          <div className=''>
+            <span className='icon icon-download' style={{ margin: '0 4px' }} />
+            <span className='icon icon-starFill' style={{ margin: '0 4px' }} />
 
-        {typePlay === 1 ? (
-          <span
-            className='icon icon-shuffle'
-            onClick={() => handleTypePlay(3)}
-          />
-        ) : typePlay === 2 ? (
-          <span
-            className='icon icon-repeat'
-            onClick={() => handleTypePlay(1)}
-          />
-        ) : typePlay === 3 ? (
-          <span className='icon icon-order' onClick={() => handleTypePlay(2)} />
-        ) : null}
-        {Number(val) === 0 ? (
-          <span className='icon icon-volumeMute' onClick={() => setVal(100)} />
-        ) : val > 0 && val <= 50 ? (
-          <span className='icon icon-volumeDown' onClick={() => setVal(0)} />
-        ) : (
-          <span className='icon icon-volume' onClick={() => setVal(0)} />
-        )}
-        <input
-          type='range'
-          min='0'
-          max='100'
-          value={val}
-          onChange={(e) => setVal(e.target.value)}
-          style={{
-            width: '80px',
-            height: '4px',
-            transform: 'translate(0, -2px)',
-          }}
-        />
-        <span style={{ fontSize: '12px' }}>{val}</span>
+            <span style={{ margin: '0 4px' }}>
+              {typePlay === 1 ? (
+                <span
+                  className='icon icon-shuffle'
+                  onClick={() => handleTypePlay(3)}
+                />
+              ) : typePlay === 2 ? (
+                <span
+                  className='icon icon-repeat'
+                  onClick={() => handleTypePlay(1)}
+                />
+              ) : typePlay === 3 ? (
+                <span
+                  className='icon icon-order'
+                  onClick={() => handleTypePlay(2)}
+                />
+              ) : null}
+            </span>
+            <span style={{ margin: '0 4px' }}>
+              {Number(val) === 0 ? (
+                <span
+                  className='icon icon-volumeMute'
+                  onClick={() => setVal(100)}
+                />
+              ) : val > 0 && val <= 50 ? (
+                <span
+                  className='icon icon-volumeDown'
+                  onClick={() => setVal(0)}
+                />
+              ) : (
+                <span className='icon icon-volume' onClick={() => setVal(0)} />
+              )}
+            </span>
+            <input
+              type='range'
+              min='0'
+              max='100'
+              value={val}
+              onChange={(e) => setVal(e.target.value)}
+              style={{
+                width: '80px',
+                height: '4px',
+              }}
+            />
+            <span style={{ fontSize: '12px', width: '20px' }}>{val}</span>
+          </div>
+        </div>
       </ContainerStyle>
     </FooterFixedMain>
   );
--- src/styles/FooterFixed.styled.jsx
+++ src/styles/FooterFixed.styled.jsx
@@ -9,4 +9,89 @@ export const FooterFixedMain = styled.div`
   bottom: 0;
   left: 0;
   color: #fff;
+  .mid2 {
+    display: none;
+    overflow: hidden;
+    .mid2Sub1 {
+      width: 50px;
+    }
+    .mid2Sub2 {
+      width: 100%;
+      clear: both;
+      display: inline-block;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+    .mid2Sub3 {
+      width: 50px;
+      padding-left: 10px;
+    }
+  }
+  @media only screen and (max-width: 576px) and (min-width: 320px) {
+    .mid2 {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      margin-bottom: 10px;
+    }
+    input[type='range'] {
+      width: 50px !important;
+    }
+  }
+  .footerFixedFlex {
+    display: flex;
+    justify-content: space-between;
+    @media only screen and (max-width: 576px) and (min-width: 320px) {
+    }
+    .mid {
+      width: 100%;
+      margin: 0 20px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 10px;
+      overflow: hidden;
+      .midSub1 {
+        width: 50px;
+      }
+      .midSub2 {
+        width: 100%;
+        clear: both;
+        display: inline-block;
+        overflow: hidden;
+        white-space: nowrap;
+      }
+      .midSub3 {
+        width: 50px;
+        padding-left: 10px;
+      }
+      @media only screen and (max-width: 576px) and (min-width: 320px) {
+        display: none;
+      }
+    }
+    div {
+      &:nth-child(1) {
+        width: 125px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+
+      @media only screen and (max-width: 576px) and (min-width: 320px) {
+        &:nth-child(3) {
+          width: 100% !important;
+          justify-content: flex-end !important;
+        }
+      }
+      &:nth-child(3) {
+        width: 358px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+    }
+  }
 `;
